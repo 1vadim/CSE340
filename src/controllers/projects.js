f7d4a403a2ac656d5ca3bf5790117021a6a28968
1vadim <vchemariev@byupathway.edu>
2026-05-30 22:27:16 +0200

    W03 Team Activity

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,10 +1,31 @@
-import { getAllProjects } from "../models/projects.js";
+import { getUpcomingProjects, getProjectDetails } from "../models/projects.js";
+
+const NUMBER_OF_UPCOMING_PROJECTS = 5;
 
 const showProjectsPage = async (req, res) => {
-  const projects = await getAllProjects();
+  const projects = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
   // console.log(projects);
-  const title = "Service Projects";
+  const title = "Upcoming Service Projects";
   res.render("projects", { title, projects });
 };
 
-export { showProjectsPage };
+const showProjectDetailsPage = async (req, res, next) => {
+  try {
+    const projectId = req.params.id;
+    const project = await getProjectDetails(projectId);
+
+    if (!project) {
+      const err = new Error("Project Not Found");
+      err.status = 404;
+      return next(err);
+    }
+
+    const title = project.title;
+
+    res.render("project", { title, project });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { showProjectsPage, showProjectDetailsPage };
