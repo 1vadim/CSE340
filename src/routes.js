e9b87a8e2f5af8de0c115ed81df7cb0da1389b2a
1vadim <vchemariev@byupathway.edu>
2026-06-05 23:26:11 +0200

    W04 Learning Activity

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,24 @@ import { showHomePage } from "./controllers/index.js";
 import {
   showOrganizationsPage,
   showOrganizationDetailsPage,
+  showNewOrganizationForm,
+  processNewOrganizationForm,
+  organizationValidation,
+  showEditOrganizationForm,
+  processEditOrganizationForm
 } from "./controllers/organizations.js";
 import {
   showProjectsPage,
   showProjectDetailsPage,
+  showNewProjectForm,
+  processNewProjectForm,
+  projectValidation
 } from "./controllers/projects.js";
 import {
   showCategoriesPage,
   showCategoryDetailsPage,
+  showAssignCategoriesForm,
+  processAssignCategoriesForm
 } from "./controllers/categories.js";
 import { testErrorPage } from "./controllers/errors.js";
 
@@ -24,9 +34,22 @@ router.get("/projects", showProjectsPage);
 router.get("/project/:id", showProjectDetailsPage);
 router.get("/categories", showCategoriesPage);
 router.get("/category/:id", showCategoryDetailsPage);
+router.get("/project/:id/assign-categories", showAssignCategoriesForm);
+router.post("/project/:id/assign-categories", processAssignCategoriesForm);
 // Route for organization details page
 router.get('/organization/:id', showOrganizationDetailsPage);
-
+// Route for new organization page
+router.get('/new-organization', showNewOrganizationForm);
+// Route to handle new organization form submission
+router.post('/new-organization', organizationValidation, processNewOrganizationForm);
+// Route to display the edit organization form
+router.get('/edit-organization/:id', showEditOrganizationForm);
+// Route to handle the edit organization form submission
+router.post("/edit-organization/:id", organizationValidation, processEditOrganizationForm);
+// Route for new project page
+router.get("/new-project", showNewProjectForm);
+// Route to handle new project form submission
+router.post("/new-project", projectValidation, processNewProjectForm);
 // error-handling routes
 router.get("/test-error", testErrorPage);
 
